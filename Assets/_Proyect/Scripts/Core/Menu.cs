f10d75867eddfaae5b9aaa31f98d5b4c034fe796
Lussio-GD <andrew.suterg@gmail.com>
2026-05-25 17:08:30 -0300

    Musica Agregada
    Musica del manu principal implementada.

--- Assets/_Proyect/Scripts/Core/Menu.cs
+++ Assets/_Proyect/Scripts/Core/Menu.cs
@@ -5,11 +5,12 @@ public class Menu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        AudioManager.Instance?.PlayMusic(SoundID.MenuMusic);
     }
-
+    
     public void MenuButton()
     {
+        AudioManager.Instance?.StopMusic();
         SceneLoader.Instance.LoadCharacterSelection();
     }
 
