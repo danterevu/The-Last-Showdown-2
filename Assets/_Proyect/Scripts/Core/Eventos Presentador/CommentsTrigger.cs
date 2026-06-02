bb21b20421bcbe972dc6dcc7adf81e4473992571
CarlosJR2 <cvilbar04@gmail.com>
2026-06-02 16:57:52 -0300

    Condiciones comentarios

--- Assets/_Proyect/Scripts/Core/Eventos Presentador/CommentsTrigger.cs
+++ Assets/_Proyect/Scripts/Core/Eventos Presentador/CommentsTrigger.cs
@@ -1,8 +1,39 @@
 public enum CommentTrigger // Nombre de eventos del presentador 
 {
+
+    // MINIGAME 1
     DodgeDiskEntry,
     PowerUpKill,
     PowerUpUsedNoKill,
-    
-    
+
+    //MINIGAME 2
+
+    PlayerDeath,
+    EnterZone,
+    AwareChangeZone,
+    Comeback,
+    AlertEndGame,
+    HumilliateRival,
+    PunchDeathRival,
+
+    //Minigame 3 
+
+    DepositDNA,
+    ThrowDnaToDeposit,
+    MineExplosion,
+    HittinWithBox,
+    AwareChangeZoneDNA,
+    BigPunchingSmall,
+    SmallPunchingBig,
+    HitSlimeShoot,
+    DoorActivated,
+
+    // Minigame 4
+
+    SabotageDeathRival,
+    SniperLaser,
+    KillWithAsteroid,
+
+
+
 }
